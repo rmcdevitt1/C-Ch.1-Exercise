8a3cfa07dfe5e58f349f11c96eca44d6e6a58f16
Raine McDevitt <raine.mcdevitt@gmail.com>
2021-07-13 15:59:44 -0500

    exercisech1

---
+++ Numeric_Types/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Numeric_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the length of your rectangle?");
+            int length = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("What is the width of your rectangle?");
+            int width = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The area of you rectangle is: " + length*width);
+
+        }
+    }
+}
