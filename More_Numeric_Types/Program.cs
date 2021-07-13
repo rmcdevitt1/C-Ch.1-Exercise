dab3162be4151892e836abe3a7e38c96b90b3812
Raine McDevitt <raine.mcdevitt@gmail.com>
2021-07-13 16:18:52 -0500

    adding strings section

---
+++ More_Numeric_Types/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace More_Numeric_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("How many miles have you driven?");
+            int miles = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("How much gas has your car consumed?");
+            int gasConsumed = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Your car's MPG is: " + miles/gasConsumed);
+        }
+    }
+}
