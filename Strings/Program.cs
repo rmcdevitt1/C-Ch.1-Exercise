178a896dce2992a4c6817e61896f400f524b42f3
Raine McDevitt <raine.mcdevitt@gmail.com>
2021-07-14 13:34:49 -0500

    finished exercises

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -6,7 +6,20 @@ namespace Strings
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool result = false;
+            string sentence = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or conversation?'";
+            Console.WriteLine("Enter a term to search for: ");
+            string answer = Console.ReadLine();
+            sentence = sentence.ToUpper();
+            answer = answer.ToUpper();
+            int tester = sentence.IndexOf(answer);
+            if(tester != -1)
+            {
+                result = true;
+            }
+
+            Console.WriteLine(result);
+
         }
     }
 }
